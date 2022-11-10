9ec06620bec83db26d4a3efad3b5a82047193be2
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2022-11-10 13:00:01 +1300

    Update morse_code_player.ino

--- morse_code_player/morse_code_player.ino
+++ morse_code_player/morse_code_player.ino
@@ -1,25 +1,27 @@
-int buzzerPin = 3;
-float wpm = 8.0;
-String string = "abcdefghijklmnopqrstuvwxyz 0123456789 , . ? \" : ' - / ( )";  // Use \" to insert double quotes.
+int buzzerPin = 3;  // The pin on the Arduino board connected to the buzzer's data pin.
+float wpm = 8.0;  // The Morse Code speed in Words Per Minute.
+String string = "Insert";  // The string to play in Morse Code.
 
-int timeScale = 1200.0 / wpm;  // Calculate the number of milliseconds to spend on a dot.
+int DOT_TIME_UNITS = 1;
+int DASH_TIME_UNITS = 3;
+int SYMBOL_GAP_TIME_UNITS = 1;
+int LETTER_GAP_TIME_UNITS = 3;
+int WORD_GAP_TIME_UNITS = 7;
 
-int dotTime = 1;
-int dashTime = 3;
-int symbolGap = 1;  // Same as dot time.
-int letterGap = 3;  // Same as three dot times.
-int wordGap = 7;
+int msPerTimeUnit = 1200.0 / wpm;
+int dotTime = DOT_TIME_UNITS * msPerTimeUnit;
+int dashTime = DASH_TIME_UNITS * msPerTimeUnit;
+int symbolGapTime = SYMBOL_GAP_TIME_UNITS * msPerTimeUnit;
+int letterGapTime = LETTER_GAP_TIME_UNITS * msPerTimeUnit;
+int wordGapTime = WORD_GAP_TIME_UNITS * msPerTimeUnit;
 
+/**
+ * Setup the buzzer control pin and play the string in Morse Code.
+ */
 void setup() {
     pinMode(buzzerPin, OUTPUT);
     digitalWrite(buzzerPin, LOW);
-
-    dotTime *= timeScale;
-    dashTime *= timeScale;
-    symbolGap *= timeScale;
-    letterGap *= timeScale;
-    wordGap *= timeScale;
-
+    
     play_string(string);
 }
 
@@ -79,157 +81,120 @@ void play_dash() {
 }
 
 void play_symbol_gap() {
-    delay(symbolGap);
+    delay(symbolGapTime);
 }
 
 void play_letter_gap() {
-    delay(letterGap);
+    delay(letterGapTime);
 }
 
 void play_word_gap() {
-    delay(wordGap);
+    delay(wordGapTime);
 }
 
+/**
+ * Translates a character into Morse Code.
+ * 
+ * @param c The character to translate into Morse Code. The character can be any uppercase or lwoercase letter, any
+ * digit, or one of the following special characters: , . ? " : ' - / ( ). If the character does not match one of these
+   characters, this function will return an empty string.
+ * 
+ * @return A string of fullstops (dots) and hyphens (dashes) representing the Morse Code.
+ */
 String translate_to_morse_code(char c) {
     if (c == 'A' || c == 'a') {
         return ".-";
-    }
-    else if (c == 'B' || c == 'b') {
+    } else if (c == 'B' || c == 'b') {
         return "-...";
-    }
-    else if (c == 'C' || c == 'c') {
+    } else if (c == 'C' || c == 'c') {
         return "-.-.";
-    }
-    else if (c == 'D' || c == 'd') {
+    } else if (c == 'D' || c == 'd') {
         return "-..";
-    }
-    else if (c == 'E' || c == 'e') {
+    } else if (c == 'E' || c == 'e') {
         return ".";
-    }
-    else if (c == 'F' || c == 'f') {
+    } else if (c == 'F' || c == 'f') {
         return "..-.";
-    }
-    else if (c == 'G' || c == 'g') {
+    } else if (c == 'G' || c == 'g') {
         return "--.";
-    }
-    else if (c == 'H' || c == 'h') {
+    } else if (c == 'H' || c == 'h') {
         return "....";
-    }
-    else if (c == 'I' || c == 'i') {
+    } else if (c == 'I' || c == 'i') {
         return "..";
-    }
-    else if (c == 'J' || c == 'j') {
+    } else if (c == 'J' || c == 'j') {
         return ".---";
-    }
-    else if (c == 'K' || c == 'k') {
+    } else if (c == 'K' || c == 'k') {
         return "-.-";
-    }
-    else if (c == 'L' || c == 'l') {
+    } else if (c == 'L' || c == 'l') {
         return ".-..";
-    }
-    else if (c == 'M' || c == 'm') {
+    } else if (c == 'M' || c == 'm') {
         return "--";
-    }
-    else if (c == 'N' || c == 'n') {
+    } else if (c == 'N' || c == 'n') {
         return "-.";
-    }
-    else if (c == 'O' || c == 'o') {
+    } else if (c == 'O' || c == 'o') {
         return "---";
-    }
-    else if (c == 'P' || c == 'p') {
+    } else if (c == 'P' || c == 'p') {
         return ".--.";
-    }
-    else if (c == 'Q' || c == 'q') {
+    } else if (c == 'Q' || c == 'q') {
         return "--.-";
-    }
-    else if (c == 'R' || c == 'r') {
+    } else if (c == 'R' || c == 'r') {
         return ".-.";
-    }
-    else if (c == 'S' || c == 's') {
+    } else if (c == 'S' || c == 's') {
         return "...";
-    }
-    else if (c == 'T' || c == 't') {
+    } else if (c == 'T' || c == 't') {
         return "-";
-    }
-    else if (c == 'U' || c == 'u') {
+    } else if (c == 'U' || c == 'u') {
         return "..-";
-    }
-    else if (c == 'V' || c == 'v') {
+    } else if (c == 'V' || c == 'v') {
         return "...-";
-    }
-    else if (c == 'W' || c == 'w') {
+    } else if (c == 'W' || c == 'w') {
         return ".--";
-    }
-    else if (c == 'X' || c == 'x') {
+    } else if (c == 'X' || c == 'x') {
         return "-..-";
-    }
-    else if (c == 'Y' || c == 'y') {
+    } else if (c == 'Y' || c == 'y') {
         return "-.--";
-    }
-    else if (c == 'Z' || c == 'z') {
+    } else if (c == 'Z' || c == 'z') {
         return "--..";
-    }
-    else if (c == '0') {
+    } else if (c == '0') {
         return "-----";
-    }
-    else if (c == '1') {
+    } else if (c == '1') {
         return ".----";
-    }
-    else if (c == '2') {
+    } else if (c == '2') {
         return "..---";
-    }
-    else if (c == '3') {
+    } else if (c == '3') {
         return "...--";
-    }
-    else if (c == '4') {
+    } else if (c == '4') {
         return "....-";
-    }
-    else if (c == '5') {
+    } else if (c == '5') {
         return ".....";
-    }
-    else if (c == '6') {
+    } else if (c == '6') {
         return "-....";
-    }
-    else if (c == '7') {
+    } else if (c == '7') {
         return "--...";
-    }
-    else if (c == '8') {
+    } else if (c == '8') {
         return "---..";
-    }
-    else if (c == '9') {
+    } else if (c == '9') {
         return "----.";
-    }
-    else if (c == ',') {
+    } else if (c == ',') {
         return "--..--";
-    }
-    else if (c == '.') {
+    } else if (c == '.') {
         return ".-.-.-";
-    }
-    else if (c == '?') {
+    } else if (c == '?') {
         return "..--..";
-    }
-    else if (c == '"') {
+    } else if (c == '"') {
         return ".-..-.";
-    }
-    else if (c == ':') {
+    } else if (c == ':') {
         return "---...";
-    }
-    else if (c == "'"[0]) {  // Gets the apostrophe at index 0 in the string.
+    } else if (c == "'"[0]) {  // Extract an apostrophe in character form.
         return ".----.";
-    }
-    else if (c == '-') {
+    } else if (c == '-') {
         return "-....-";
-    }
-    else if (c == '/') {
+    } else if (c == '/') {
         return "-..-.";
-    }
-    else if (c == '(') {
+    } else if (c == '(') {
         return "-.--.";
-    }
-    else if (c == ')') {
+    } else if (c == ')') {
         return "-.--.-";
-    }
-    else {
+    } else {
         return "";
     }
 }
